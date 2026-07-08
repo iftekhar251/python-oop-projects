4e1ccac765e00425494df4c41446d9372591f465
Iftekhar Uddin Akil <iftekharuddinakil8@gmail.com>
2026-07-08 17:00:33 +0600

    Add files via upload

---
+++ Ride Share/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+from ride import RideSharing,Ride,RideRequest,RideMatching
+from users import Rider,Driver
+from vehicle import Car,Bike
+
+niye_jao=RideSharing("Niye jao")
+rahim=Rider("Rahim uddin","rahim2@gamil.com",1245,"banani",500)
+niye_jao.add_rider(rahim)
+karim=Driver("karim uddin","karim22@gamil.com",145245,"gulshan")
+niye_jao.add_driver(karim)
+rahim.request_ride(niye_jao,"uttara","car")
+rahim.show_current_ride()
+karim.reach_destination(rahim.current_ride)
+print(niye_jao)
